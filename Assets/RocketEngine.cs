b0765470fba2be8105ae3cfc7c74e5486a73c941
denise <silentfront@gmail.com>
2017-11-27 05:36:29 -0800

    first commit

---
+++ Assets/RocketEngine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class RocketEngine : MonoBehaviour {
+
+    public float fuelMass;             // [kg]
+    public float maxThrust;            // kN [kg*m*s^-2]
+
+    [Range(0, 1f)]
+    public float thrustPercent;        // [none]
+    public Vector3 thrustUnitVector;   // [none] -- a direction 
+
+    private PhysicsEngine physicsEngine;
+    private float currentThrust;       // N
+    private float netMassInitial;
+    private float netMassFinal;
+
+    void Start()
+    {
+        physicsEngine = gameObject.GetComponent<PhysicsEngine>();
+        netMassFinal = physicsEngine.mass;
+        physicsEngine.mass += fuelMass;
+        netMassInitial = physicsEngine.mass;
+    }
+
+    void FixedUpdate()
+    {
+        float fuelMassThisUpdate = FuelThisUpdate();
+        if (fuelMass >= fuelMassThisUpdate)
+        {
+            fuelMass -= fuelMassThisUpdate;
+            physicsEngine.mass -= fuelMassThisUpdate;
+            ExertForce();
+        }
+        else {
+            Debug.LogWarning("Out of rocket fuel.");
+        }
+    }
+
+    float FuelThisUpdate() {
+        float exhaustMassFlow;                     // [kg s^-1]
+        float effectiveExhaustVelocity;            // [m s^-1]
+
+        effectiveExhaustVelocity = 4464f;          // [m s^-1] for a liquid Oxygen/Liquid Hydrogen Engine
+        exhaustMassFlow = currentThrust / effectiveExhaustVelocity; 
+        return exhaustMassFlow * Time.deltaTime;   // [kg]
+    }
+
+    void ExertForce() {
+        currentThrust = thrustPercent * maxThrust * 1000f;
+        Vector3 thrustVector = thrustUnitVector.normalized * currentThrust; // N
+        physicsEngine.AddForce(thrustVector);
+    }
+}
---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour {
+    public float mass;                  // [kg] 
+    public Vector3 velocityVector;      // Average velocityVector this FixedUpdate() [m s^-1]
+    public Vector3 netForceVector;      // N [kg m s^-2]
+    public bool showTrails = true;
+
+    
+
+    private List<Vector3> forceVectorList = new List<Vector3>();
+    private LineRenderer lineRenderer;
+    private int numberOfForces;
+    private Vector3 deltaS;            // [m]
+    private PhysicsEngine[] physicsEngineArray;
+    private Vector3 gravitationalForcevector;
+    // Use this for initialization
+    void Start() {
+        deltaS = new Vector3(0, 0, 0);
+        StartDrawForces();
+        physicsEngineArray = FindObjectsOfType<PhysicsEngine>();
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        UpdateDrawForces();
+        CalculateGravity();
+        UpdatePosition();
+    }
+
+    public void AddForce(Vector3 forceVector)
+    {
+        forceVectorList.Add(forceVector);
+    }
+
+    void CalculateGravity()
+    {
+        float G = .000006674f;
+        gravitationalForcevector = Vector3.zero; 
+
+        foreach (PhysicsEngine body in physicsEngineArray) {
+            if (!body.Equals(this) && (gameObject.transform.position - body.transform.position).magnitude >= 1f) {
+                Vector3 r = gameObject.transform.position - body.transform.position;
+
+                //Debug.Log((G * mass * body.mass / Mathf.Pow(r.magnitude, 2)) * r.normalized);
+                gravitationalForcevector += -(G * mass * body.mass / Mathf.Pow(r.magnitude, 2)) * r.normalized;
+
+            }
+
+        }
+        //Debug.Log(gravitationalForcevector);
+    }
+
+    void UpdatePosition() {
+        // Sum forces and clear list
+        netForceVector = Vector3.zero;
+
+        foreach (Vector3 forceVector in forceVectorList)
+        {
+            netForceVector += forceVector;
+        }
+        forceVectorList.Clear();
+
+        // Update velocity
+        Vector3 accelerationVector = (netForceVector + gravitationalForcevector) / mass;
+        Debug.Log(accelerationVector);
+        velocityVector += accelerationVector * Time.deltaTime;
+        // Update Position
+        deltaS = velocityVector * Time.deltaTime;
+        gameObject.transform.position += deltaS;
+    }
+
+    void StartDrawForces() {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.yellow;
+        lineRenderer.endColor = Color.yellow;
+        lineRenderer.startWidth = 0.2F;
+        lineRenderer.endWidth = 0.2F;
+        lineRenderer.useWorldSpace = false;
+    }
+
+    void UpdateDrawForces() {
+        if (showTrails)
+        {
+            lineRenderer.enabled = true;
+            numberOfForces = forceVectorList.Count;
+            lineRenderer.positionCount = numberOfForces * 2;
+            int i = 0;
+            foreach (Vector3 forceVector in forceVectorList)
+            {
+                lineRenderer.SetPosition(i, Vector3.zero);
+                lineRenderer.SetPosition(i + 1, -forceVector);
+                i = i + 2;
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+}
